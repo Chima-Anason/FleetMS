0df229435cc914fb9e7be6522b3678fcdfeadcfb
Chima-Anason <tblynxx@gmail.com>
2022-07-14 23:26:36 +0800

    Set Up the 6 models in the template and assign them to a handle method in ApplicationController

--- src/main/java/com/anagraceTech/FleetMS/ApplicationController.java
+++ src/main/java/com/anagraceTech/FleetMS/ApplicationController.java
@@ -8,15 +8,49 @@ public class ApplicationController {
 
 	
 	
-	@RequestMapping("/layout")
+	@RequestMapping("/index")
 	public String getIndexPage() {
 
-		return "_layout";
+		return "index";
 	}
 
-	@RequestMapping("/index")
-	public String getlayout() {
+	@RequestMapping("/hr")
+	public String hr() {
 
-		return "index2";
+		return "/hr/index";
+	}
+	
+	@RequestMapping("/fleet")
+	public String fleet() {
+
+		return "/fleet/index";
+	}
+	
+	
+	@RequestMapping("/accounts")
+	public String accounts() {
+
+		return "/accounts/index";
+	}
+	
+	
+	@RequestMapping("/helpdesk")
+	public String helpdesk() {
+
+		return "/helpdesk/index";
+	}
+	
+	
+	@RequestMapping("/parameters")
+	public String parameters() {
+
+		return "/parameters/index";
+	}
+	
+	
+	@RequestMapping("/payroll")
+	public String payroll() {
+
+		return "/payroll/index";
 	}
 }
